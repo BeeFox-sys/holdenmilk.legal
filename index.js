a255686197c23d12696bc2b3e6e0e7f9b0b2a115
Fiona <fiona.greenwood.sheridan@gmail.com>
2020-10-22 14:14:26 +1100

    init commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const express = require("express");
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+const { Liquid } = require("liquidjs");
+const { query } = require("express");
+const engine = new Liquid({
+    cache: process.env.NODE_ENV === "PROD",
+    extname: ".html" 
+});
+
+app.engine("html", engine.express()); 
+app.set("views", __dirname+"\\views");
+app.set("view engine", "liquid");
+
+app.use(express.static("static"));
+
+app.get("/",async (req, res)=>{
+    res.render("index.html");
+});
+app.get("/login",async (req, res)=>{
+    res.render("login.html");
+});
+
+app.get("/client", async (req,res)=>{
+    if(!req.query?.username || !req.query?.password) res.render("fail.html")
+
+    switch (req.query.username) {
+        case "gunther_obrian":
+                if(req.query.password != "abc123") return res.render("fail.html");
+                res.render("clients\\gunther_obrian.html",)
+            break;
+    
+        default:
+            res.render("fail.html")
+            break;
+    }
+})
+
+let port = process.env.PORT ?? 8000;
+app.listen(port, () => {
+    console.log("Ready on",port);
+});
