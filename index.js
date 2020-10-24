2a2c4e923c9787099e42957723d81d69de11e888
Fiona <fiona.greenwood.sheridan@gmail.com>
2020-10-24 11:16:26 +1100

    site update

--- index.js
+++ index.js
@@ -36,6 +36,46 @@ app.get("/client", async (req,res)=>{
             if(req.query.password != "whatareyouacop") return res.render("fail.html");
             res.render("clients/ch_shoethieves.html",)
         break;
+        case "stu_trololol":
+            if(req.query.password != "NoTheOtherStu") return res.render("fail.html");
+            res.render("clients/stu_trololol.html",)
+        break;
+        case "simon_haley":
+            if(req.query.password != "PENTA") return res.render("fail.html");
+            res.render("clients/simon_haley.html",)
+        break;
+        case "velazquez_aslott":
+            if(req.query.password != "twofer") return res.render("fail.html");
+            res.render("clients/velazquez_aslott.html",)
+        break;
+        case "blood_hamburger":
+            if(req.query.password != "BludeHamberzher") return res.render("fail.html");
+            res.render("clients/blood_hamburger.html",)
+        break;
+        case "hotbox_sato":
+            if(req.query.password != "IDTCITSGIAGI") return res.render("fail.html");
+            res.render("clients/hotbox_sato.html",)
+        break;
+        case "sebastian_townsend":
+            if(req.query.password != "NotSkeletor") return res.render("fail.html");
+            res.render("clients/sebastian_townsend.html",)
+        break;
+        case "howell_franklin":
+            if(req.query.password != "AWOOO") return res.render("fail.html");
+            res.render("clients/howell_franklin.html",)
+        break;
+        case "snyder_briggs":
+            if(req.query.password != "360cKICK") return res.render("fail.html");
+            res.render("clients/snyder_briggs.html",)
+        break;
+        case "fitzgerald_wanderlust":
+            if(req.query.password != "Sophia") return res.render("fail.html");
+            res.render("clients/fitzgerald_wanderlust.html",)
+        break;
+        case "tillman_henderson":
+            if(req.query.password != "ERR58008") return res.render("fail.html");
+            res.render("clients/tillman_henderson.html",)
+        break;
         default:
             res.render("fail.html")
             break;
